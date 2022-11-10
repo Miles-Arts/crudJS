3e34a0b672055722e10dec1fc5c08d26fe5760ea
Milton Figueredo Miles <gratno@gmail.com>
2022-11-10 11:03:50 -0500

    Html en JS

--- service/client-service.js
+++ service/client-service.js
@@ -1,22 +1,46 @@
-const http = new XMLHttpRequest()
+const crearNuevaLinea = () => {
 
-//abrir http (método, url);
+    const contenido = `</tr >
+    </thead >
+    <tbody data-table>
+        <tr>
+            <td class="td" data-td>Gabriela</td>
+            <td>gabriela@alura.com</td>
+            <td>
+                <ul class="table__button-control">
+                    <li>
+                        <a href="../screens/editar_cliente.html" class="simple-button simple-button--edit">Editar</a>
+                    </li>
+                    <li>
+                        <button class="simple-button simple-button--delete" type="button">
+                            Eliminar
+                        </button>
+                    </li>
+                </ul>
+            </td>
+        </tr>`;
+    };
 
-//Crud  -método HTTP
-//Create  - Post
-//Read  - Get
-//Update  - Put/Patch
-//Delete  - Delete
 
-http.open("GET", "http://localhost:3000/perfil");
+const http = new XMLHttpRequest();
 
-http.send();
+        //abrir http (método, url);
+
+        //Crud  -método HTTP
+        //Create  - Post
+        //Read  - Get
+        //Update  - Put/Patch
+        //Delete  - Delete
+
+        http.open("GET", "http://localhost:3000/perfil");
+
+        http.send();
 
 http.onload = () => {
 
     const data = http.response
-    console.log(data);
+        console.log(data);
 
 };
 
-console.log(http);
+        console.log(http);
