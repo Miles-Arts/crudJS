868dd55b4138c63d2b6a29d93916c38eb93afd4a
Milton Figueredo Miles <gratno@gmail.com>
2022-11-14 18:50:40 -0500

    Usando libreria uuid-v4

--- service/client-service.js
+++ service/client-service.js
@@ -7,8 +7,20 @@
 //fecth API
 const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
+const crearCliente = (nombre , email) => {
+    fetch("http://localhost:3000/perfil", {
+        method: "POST",
+        header: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nombre, email, id: uuid.v4()})
+    });
+}
+
+
 export const clientServices = {
-    listaClientes
+    listaClientes,
+    crearCliente,
 }
 
 
