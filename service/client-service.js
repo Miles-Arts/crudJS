92045379d37b469046def24443bc62ed2ff95e8f
Milton Figueredo Miles <gratno@gmail.com>
2022-11-14 14:39:35 -0500

    Separando creando nuevas carpetas

--- service/client-service.js
+++ service/client-service.js
@@ -1,29 +1,3 @@
-const crearNuevaLinea = (nombre, email) => {
-
-    const linea = document.createElement("tr")
-    const contenido = `
-    
-            <td class="td" data-td>${nombre}</td>
-            <td>${email}</td>
-            <td>
-                <ul class="table__button-control">
-                    <li>
-                        <a href="../screens/editar_cliente.html" class="simple-button simple-button--edit">Editar</a>
-                    </li>
-                    <li>
-                        <button class="simple-button simple-button--delete" type="button">
-                            Eliminar
-                        </button>
-                    </li>
-                </ul>
-            </td>
-        `;
-    linea.innerHTML = contenido;
-    return linea
-};
-
-const table = document.querySelector("[data-table]")
-
 //abrir http (método, url);
 //Crud  -método HTTP
 //Create  - Post
@@ -33,14 +7,8 @@ const table = document.querySelector("[data-table]")
 //fecth API
 const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
-
-listaClientes()
-    .then((data) => {
-
-        data.forEach((perfil) => {
-            const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-            table.appendChild(nuevaLinea);
-        });
-    }).catch((error) => alert("Ocurrio un error"));
+export const clientServices = {
+    listaClientes
+}
 
 
