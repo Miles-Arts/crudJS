f98fce76bba099eec73ac606225a1fa90a7d7a16
Milton Figueredo Miles <gratno@gmail.com>
2022-11-15 18:32:26 -0500

    Limpiando code

--- service/client-service.js
+++ service/client-service.js
@@ -18,7 +18,6 @@ const crearCliente = (nombre, email) => {
 };
 
 const eliminarCliente = (id) => {
-    console.log("Eliminar a: " , id)
     return fetch(`http://localhost:3000/perfil/${id}`, {
         method: "DELETE",
 
